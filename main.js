6c0a17c1ed25546a1ff0019edce43a5f8910d448
iguinz <130095170+iguinz@users.noreply.github.com>
2023-08-15 17:52:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML = " ";
+    recognition.start(); 
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+    speak();
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speakData = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+
+});
